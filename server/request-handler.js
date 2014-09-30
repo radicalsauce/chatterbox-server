c1216a05b842a40a7b2f86af9f1bac5410dd98c2
Andrew Selbie <aselbie@gmail.com>
2014-09-30 10:02:19 -0700

    Initial work

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,45 +1,82 @@
-/* You should implement your request handler function in this file.
- * And hey! This is already getting passed to http.createServer()
- * in basic-server.js. But it won't work as is.
- * You'll have to figure out a way to export this function from
- * this file and include it in basic-server.js so that it actually works.
- * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var parseUrl = require('url').parse;
+
+var idCounter = 0;
+var messageStore = [
+  {
+    id: idCounter++,
+    username: 'Bob',
+    text: 'Hello world',
+    createdAt: '2014-09-29T22:35:20.481Z',
+    updatedAt: '2014-09-29T22:35:20.481Z'
+  }
+];
+
+var filterMessages = function(messages, filters) {
+  filters = JSON.parse(filters);
+  for (var prop in filters) {
+    if (filters[prop]['$gt']) {
+      if (filters[prop]['$gt']['__type'] && filters[prop]['$gt']['iso'])
+      messages = messages.filter(function(message){
+        if (Date(message.updatedAt)) {
+
+        }
+      })
+    }
+  }
+  return messages;
+}
+
+var orderMessages = function(messages, order) {
+  var reverse = false;
+  if (order.charAt(0) === '-') {
+    reverse = true;
+    order.shift();
+  }
+  return messages.sort(function(a,b){
+  });
+}
+
+var getMessages = function(filters) {
+  var messages = messageStore;
+
+  console.log(filters);
+
+  // if (filters.where) {
+  //   messages = filterMessages(messages, filters.where);
+  // }
+
+  // if (filters.order) {
+  //   messages = orderMessages(messages, filters.order);
+  // }
+
+  return messages;
+}
 
 var handleRequest = function(request, response) {
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as what URL the browser is requesting. */
+  var pathParts = parseUrl(request.url).pathname.split('/');
+  var error = '';
 
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
-
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
-  var statusCode = 200;
-
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
   var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/json";
 
-  headers['Content-Type'] = "text/plain";
+  // Reject the request if the URL is not the one we're looking for
+  if (pathParts[1] !== '1' || pathParts[2] !== 'classes' || pathParts[3] !== 'chatterbox') {
+    response.writeHead(404, headers);
+    return response.end();
+  }
 
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
+  if (request.method === 'GET') {
+    response.writeHead(200, headers);
+    response.end(JSON.stringify(getMessages(parseUrl(request.url, true).query)));
+  }
 
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end("Hello, World!");
 };
 
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
- * This CRUCIAL code allows this server to talk to websites that
- * are on different domains. (Your chat client is running from a url
- * like file://your/chat/client/index.html, which is considered a
- * different domain.) */
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+
+module.exports = handleRequest;
